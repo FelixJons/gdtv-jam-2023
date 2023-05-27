e0d9f7d85afc9c81376f7318710caade2e587084
FelixJ <feljon@student.chalmers.se>
2023-05-27 22:02:33 +0200

    Added Dialogic

--- game/world.gd
+++ game/world.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$EnemySpawner.set_current_level("Level1")
 	$EnemySpawner.spawn()
-
+	get_window().set_content_scale_size(Vector2i(24*16,24*16))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
