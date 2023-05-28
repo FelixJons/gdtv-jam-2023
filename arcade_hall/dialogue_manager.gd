230ca23f9d0b150d1681ee24c109dad14156f199
Felix J <felix.jonsson1993@gmail.com>
2023-05-28 13:54:13 +0200

    Scene chain is complete

--- arcade_hall/dialogue_manager.gd
+++ arcade_hall/dialogue_manager.gd
@@ -1,22 +1,22 @@
 extends Node
 
-class_name DialogueManager
+var start_dialogue_file_path = "res://dialogue_data/start_dialogue.json"
+var start_dialogue_dict: Dictionary
 
 func _ready():
-	load_dialogue(2)
+	start_dialogue_dict = load_dialogue(start_dialogue_file_path)
 
 func load_dialogue(file_path) -> Dictionary:
-	file_path = "res://dialogue_data/start_dialogue.json"
+	var json_as_dict
 	if FileAccess.file_exists(file_path):
 		var json_as_text = FileAccess.get_file_as_string(file_path)
-		var json_as_dict = JSON.parse_string(json_as_text)
+		json_as_dict = JSON.parse_string(json_as_text)
 		if json_as_dict:
 			print(json_as_dict)
 			print(json_as_dict["001"]["text"])
-	return Dictionary()
+	return json_as_dict
 
-
-"""var json_as_text = FileAccess.get_file_as_string(file)
-var json_as_dict = JSON.parse_string(json_as_text)
-if json_as_dict:
-	print(json_as_dict)"""
+func start_dialogue() -> Array:
+	var char = start_dialogue_dict["001"]["char"]
+	var text = start_dialogue_dict["001"]["text"]
+	return [char,text]
---
+++ arcade_hall/start_dialogue.gd
@@ -0,0 +1,51 @@
+extends Sprite2D
+
+@onready var text_box := $DialogueTextLabel
+@onready var fem_dialogue := $FemaleDialogueBox
+@onready var male_dialogue := $MaleDialogueBox
+@export var game_scene: PackedScene
+var dialogue_dict: Dictionary
+var text_speed: float = 0.05
+var is_loading_dialogue = false
+var current_dialogue_id = "001"
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fem_dialogue.visible = false
+	male_dialogue.visible = false
+	
+	dialogue_dict = Dialogue.start_dialogue_dict
+	
+	# First dialogue step
+	display_dialogue(current_dialogue_id)
+	
+func _process(delta):
+	if not is_loading_dialogue and Input.is_action_just_pressed("item"):
+		current_dialogue_id = increment_string_number_by_one(current_dialogue_id)
+		display_dialogue(current_dialogue_id)
+
+func set_active_char(char: String) -> void:
+	if char == "female_duck":
+		fem_dialogue.visible = true
+		male_dialogue.visible = false
+	elif char == "male_duck":
+		fem_dialogue.visible = false
+		male_dialogue.visible = true
+		
+func display_dialogue(dialogue_id: String) -> void:
+	if dialogue_dict[dialogue_id]["type"] == "breakpoint":
+		get_tree().change_scene_to_packed(game_scene)
+		return
+
+	is_loading_dialogue = true
+	set_active_char(dialogue_dict[dialogue_id]["char"])
+	text_box.clear()
+	var dialogue_text = dialogue_dict[dialogue_id]["text"]
+	for char in dialogue_text:
+		text_box.add_text(char)
+		await get_tree().create_timer(text_speed).timeout
+	is_loading_dialogue = false
+	
+func increment_string_number_by_one(s: String) -> String:
+	var number = int(s) # convert the string to int
+	number += 1 # increment the number
+	return str(number).pad_zeros(3) # convert back to string and return
