2db99842103a7bfb345c4b3cb47173a5e1b2c6c3
Felix J <felix.jonsson1993@gmail.com>
2023-05-29 13:25:41 +0200

    Remove unused files

--- shaders/tv_shader_tween.gd
+++
@@ -1,9 +0,0 @@
-extends ColorRect
-
-func _ready():
-	var tween = get_tree().create_tween()
-	tween.tween_method(set_shader_value, 0.0, 1.0, 2)
-	
-func set_shader_value(value: float):
-	material.set_shader_parameter("progress", value)
-	
